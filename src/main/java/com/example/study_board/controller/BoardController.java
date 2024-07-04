fa42ee968d896dd2c1683285ab649f5f003ae7e2
Jin <jinnkimm7@gmail.com>
2024-07-04 11:17:37 +0900

    03_게시글 작성화면 구현

---
+++ src/main/java/com/example/study_board/controller/BoardController.java
@@ -0,0 +1,17 @@
+package com.example.study_board.controller;
+
+import com.example.study_board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+}
---
+++ src/main/java/com/example/study_board/service/BoardService.java
@@ -0,0 +1,11 @@
+package com.example.study_board.service;
+
+import com.example.study_board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+}
