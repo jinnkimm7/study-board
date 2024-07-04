7eaef1c167ed0d0ebdd1fe7def1f81036cfc6207
Jin <jinnkimm7@gmail.com>
2024-07-04 14:13:53 +0900

    게시글 목록 출력 메서드 및 화면 구현

--- src/main/java/com/example/study_board/controller/BoardController.java
+++ src/main/java/com/example/study_board/controller/BoardController.java
@@ -4,8 +4,11 @@ import com.example.study_board.dto.BoardDTO;
 import com.example.study_board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,5 +24,12 @@ public class BoardController {
     public String save(BoardDTO boardDTO) {
         boardService.save(boardDTO);
         return "index";
+    }
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
     }
 }
